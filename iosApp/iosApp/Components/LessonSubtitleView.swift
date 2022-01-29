00b899e7c1b0f7ad8bc53bc2f5fc8329d8bed616
zotovy <m@zotov.dev>
2022-01-29 12:01:26 +0500

    feat(ios): create LessonSubtitleView component

---
+++ iosApp/iosApp/Components/LessonSubtitleView.swift
@@ -0,0 +1,46 @@
+//
+//  LessonSubtitleView.swift
+//  iosApp
+//
+//  Created by Yaroslav Zotov on 29.01.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct LessonSubtitleView: View {
+    let text: String
+    
+    init(_ text: String) {
+        self.text = text
+    }
+    
+    var body: some View {
+        Text(text)
+            .font(.custom("AvenirNext-Regular", size: 14))
+            .foregroundColor(Color.secondary)
+            .lineLimit(1)
+    }
+}
+
+struct LessonSubtitleDotView: View {
+    var body: some View {
+        Circle()
+            .padding(10)
+            .frame(width: 3, height: 3)
+            .background(Color.secondary)
+    }
+}
+
+struct LessonSubtitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            LessonSubtitleView("Какой-то сабтайтл")
+            
+            HStack {
+                LessonSubtitleDotView()
+                LessonSubtitleView("Какой-то сабтайтл c точкой")
+            }
+        }
+    }
+}
