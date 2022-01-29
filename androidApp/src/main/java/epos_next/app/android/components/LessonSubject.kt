e7712f83e8532afcaec224adffdb1d8450b6cb9f
zotovy <m@zotov.dev>
2022-01-29 11:55:43 +0500

    perf(android): remove unnecessary Box from LessonSubject component

--- androidApp/src/main/java/epos_next/app/android/components/LessonSubject.kt
+++ androidApp/src/main/java/epos_next/app/android/components/LessonSubject.kt
@@ -1,6 +1,5 @@
 package epos_next.app.android.components
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,14 +12,13 @@ import epos_next.app.android.helpers.UiHelper
 
 @Composable
 fun LessonSubject(modifier: Modifier = Modifier, text: String) {
-    Box(modifier = modifier) {
-        Text(
-            text = UiHelper.formatSubjectName(text),
-            style = TextStyle(
-                fontWeight = FontWeight.W400,
-                fontSize = 16.sp,
-                color = MaterialTheme.colors.textPrimary,
-            )
-        )
-    }
+    Text(
+        text = UiHelper.formatSubjectName(text),
+        style = TextStyle(
+            fontWeight = FontWeight.W400,
+            fontSize = 16.sp,
+            color = MaterialTheme.colors.textPrimary,
+        ),
+        modifier = modifier
+    )
 }
