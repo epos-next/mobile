be429f8e369fabe3c43bb8ccad4a8fdb59dc8fe7
Yaroslav Zotov <35657233+zotovy@users.noreply.github.com>
2022-01-31 15:36:12 +0500

    feat(ios): create SearchInputView

---
+++ iosApp/iosApp/Features/Marks/Main/MarksScreenView.swift
@@ -0,0 +1,23 @@
+//
+//  MarksScreenView.swift
+//  iosApp
+//
+//  Created by Yaroslav Zotov on 31.01.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct MarksScreenView: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MarksScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        MarksScreenView()
+    }
+}
