664fcb6ab0fd0ffcba91e054ce7d2d60e7432f3b
Yaroslav Zotov <35657233+zotovy@users.noreply.github.com>
2022-03-10 08:19:56 +0500

    feat(android): implement dark mode at bottom nav bar

--- androidApp/src/main/java/epos_next/app/android/components/BottomNavBar.kt
+++ androidApp/src/main/java/epos_next/app/android/components/BottomNavBar.kt
@@ -3,23 +3,31 @@ package epos_next.app.android.components
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import epos_next.app.android.R
+import epos_next.app.android.components.theme.lightPrimary
 import epos_next.app.android.navigation.Routes
 
 @Composable
 fun BottomNavBar(navController: NavHostController) {
     BottomNavigation(
-        backgroundColor = MaterialTheme.colors.background,
+        backgroundColor = if (MaterialTheme.colors.isLight) MaterialTheme.colors.background else Color(
+            0xFF1B1822
+        ),
+        modifier = Modifier.clip(RoundedCornerShape(20.dp, 20.dp, 0.dp, 0.dp))
     ) {
         val route = currentRoute(navController)
 
@@ -29,7 +37,8 @@ fun BottomNavBar(navController: NavHostController) {
                     Image(
                         painter = painterResource(id = item.getIcon(route)),
                         contentDescription = item.description,
-                        modifier = Modifier.size(32.dp)
+                        modifier = Modifier.size(32.dp),
+                        colorFilter = if (!item.selected(route)) ColorFilter.tint(Color(0xFF83848D)) else null,
                     )
                 },
                 selected = item.selected(route),
