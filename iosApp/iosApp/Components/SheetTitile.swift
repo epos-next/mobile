cb46a76432ee7f8e759bd2013e6a40fe7508918c
Yaroslav Zotov <35657233+zotovy@users.noreply.github.com>
2022-03-23 14:16:26 +0500

    feat(ios): create SheetTitleView

---
+++ iosApp/iosApp/Components/SheetTitile.swift
@@ -0,0 +1,21 @@
+//
+//  SheetTitile.swift
+//  iosApp
+//
+//  Created by Yaroslav Zotov on 23.03.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SheetTitile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SheetTitile_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetTitile()
+    }
+}
---
+++ iosApp/iosApp/Components/SheetTitleView.swift
@@ -0,0 +1,31 @@
+//
+//  SheetTitleView.swift
+//  iosApp
+//
+//  Created by Yaroslav Zotov on 23.03.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct SheetTitleView: View {
+    let text: String
+    
+    init (_ text: String) {
+        self.text = text
+    }
+    
+    var body: some View {
+        Text(text)
+            .font(.custom("AvenirNext-Medium", size: 16))
+            .multilineTextAlignment(.center)
+            .foregroundColor(Color.textPrimary)
+            .padding(.top, 20)
+    }
+}
+
+struct SheetTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetTitleView("Новая контрольная работа")
+    }
+}
