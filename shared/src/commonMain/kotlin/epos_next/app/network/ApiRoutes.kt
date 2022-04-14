a4891d26c0e0e4c8d2d9b25d1a77567adbf47d9d
Yaroslav Zotov <35657233+zotovy@users.noreply.github.com>
2022-04-14 21:40:06 +0500

    feat(android): add weight to mark
    CU-2bukqzr

--- shared/src/commonMain/kotlin/epos_next/app/network/ApiRoutes.kt
+++ shared/src/commonMain/kotlin/epos_next/app/network/ApiRoutes.kt
@@ -6,7 +6,7 @@ object ApiRoutes {
     const val authenticate = "/api/1.1/auth/authenticate"
     const val reauthenticate = "/api/1.0/auth/reauthenticate"
 
-    const val data = "/api/1.1/data"
+    const val data = "/api/1.2/data"
     fun fetchLesson(from: String, to: String) = "/api/1.1/data/lessons?from=$from&to=$to"
     fun completeHomework(id: Long) = "/api/1.0/homework/$id/complete"
     fun cancelCompleteHomework(id: Long) = "/api/1.0/homework/$id/cancel-complete"
